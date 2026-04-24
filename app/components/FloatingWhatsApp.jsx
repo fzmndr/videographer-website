3c37d463a88078dafd19567ff27186aa314b8ed9
Dika-Doki <dikadoki3@gmail.com>
2026-04-25 02:32:04 +0700

    update website

--- app/components/FloatingWhatsApp.jsx
+++ app/components/FloatingWhatsApp.jsx
@@ -4,7 +4,7 @@ const WHATSAPP_NUMBER = "6285775355771";
 const WHATSAPP_TEXT =
   "Halo Dika Doki, saya tertarik dengan jasa videography. Bisa info paket dan harga?";
 
-const INSTAGRAM_URL = "https://instagram.com/username_kamu";
+const INSTAGRAM_URL = "https://www.instagram.com/dikadoki/";
 
 export default function FloatingSocial() {
   const whatsappUrl = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(
