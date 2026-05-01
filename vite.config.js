1378afb662367828d32bccace7abb9bcfcd2ad65
Dika-Doki <dikadoki3@gmail.com>
2026-05-01 13:27:25 +0700

    update website

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Tambahkan konfigurasi legacy di sini
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app'),
